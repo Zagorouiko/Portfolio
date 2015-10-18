be948bb6d0a2395971fe3cc0fec871fa0e8adf4f
Leon <Zagorouiko@gmail.com>
2015-10-17 19:10:19 -0700

    changed root to front page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    authenticated :user do
-      root 'categories#index', as: :authenticated_root
+      root 'categories#index'
     end
-
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
 
   resources :categories do
     resources :projects, except: [:index]
